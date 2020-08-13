0feaafcc93aa05fdf20ad0556ea1ce71bbfbba23
MaximeGrmd <47553450+MaximeGrmd@users.noreply.github.com>
2020-08-13 21:25:15 +0200

    [App] Customize back bindings with agile app.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Agile App Demo',
       theme: ThemeData(
         // This is the theme of the application.
         primarySwatch: Colors.deepPurple,
