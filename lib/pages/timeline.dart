6a476d233320372ef98af9ef92174cd9ab689087
MaximeGrmd <47553450+MaximeGrmd@users.noreply.github.com>
2020-08-15 10:16:55 +0200

    [Timeline] Add Stream builder to display a dynamic list of username.

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_agile_app/widgets/header.dart';
+import 'package:flutter_agile_app/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,10 +12,39 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    // getUserById();
+    super.initState();
+  }
+
+  // getUserById() async {
+  //   final String id = "MXu9OVGJtVpEXY2oWIX9";
+  //   final DocumentSnapshot doc = await usersRef.document(id).get();
+  //   print(doc.data);
+  //   print(doc.documentID);
+  //   print(doc.exists);
+  // }
+
+  @override
   Scaffold build(context) {
     return Scaffold(
-      appBar: header(context),
-      body: Text("Timeline"),
+      appBar: header(context, isAppTitle: true),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: usersRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Text> children = snapshot.data.documents
+              .map((doc) => Text(doc['username']))
+              .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 }
